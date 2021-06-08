73e65300b20a53e3816cc4629081499c5903e415
Simon Elvery <simon@elvery.net>
2021-06-08 11:20:16 +1000

    Reject the promise if proxy is implemented

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 // Checks if a proxy is requested for this project and loads it if required.
 // The returned promise will only resolve if no proxy is loaded.
 export const proxy = (project: string) =>
-  new Promise<void>(resolve => {
+  new Promise<void>((resolve, reject) => {
     // If we're already in a dev environment, there's nothing to do here.
     if (process.env.NODE_ENV === 'development') return resolve();
     // Never run on live/production.
     if (document.location.host === 'www.abc.net.au') return resolve();
 
-    let proxied = false;
     const src = localStorage.getItem('proxy_' + project);
 
     if (src) {
       const scr = document.createElement('script');
       scr.src = src;
       document.head.appendChild(scr);
-      proxied = true;
-      console.info('[dev-proxy] Loaded script: ' + src + ` (${project})`);
+      const msg = '[dev-proxy] Loaded script: ' + src + ` (${project})`;
+      console.info(msg);
+      return reject(msg);
     }
 
-    if (!proxied) resolve();
+    resolve();
   });
